27e88d14014fbb2fd50e5f5f76d07581fca0254b
leonardo <leoescuer@protonmail.com>
2023-08-06 11:18:15 +0200

    require file name change

--- packages/secp256k1/lib/index.ts
+++ packages/secp256k1/lib/index.ts
@@ -11,6 +11,6 @@ export * from "./Secp256k1";
 import path from "path";
 import { Secp256k1 } from "./Secp256k1";
 
-const addon = require(path.resolve(__dirname, "../build/Release/addon"));
+const addon = require(path.resolve(__dirname, "../build/Release/addon.node"));
 const instance: Secp256k1 = new Secp256k1(new addon.Secp256k1());
 export default instance;
